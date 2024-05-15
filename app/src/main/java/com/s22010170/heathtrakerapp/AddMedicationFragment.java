051e85aa636aa086cd022d09c295565a8f6c342b
kavinda <kavindarathnayake100@gmail.com>
2024-05-15 21:02:50 +0530

    add, edit medication layouts and it's functionality.

--- app/src/main/java/com/s22010170/heathtrakerapp/AddMedicationFragment.java
+++ app/src/main/java/com/s22010170/heathtrakerapp/AddMedicationFragment.java
@@ -7,14 +7,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class AddMedicationFragment extends Fragment {
+
+    Spinner spinner;
+    Button addMedicationButton, cancelMedicationButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_add_medication, container, false);
 
+        // define am_pm array
+        String[] am_pm = {"AM", "PM"};
+
+        // define variables
+        spinner = rootView.findViewById(R.id.add_medication_frequency_input_am_pm);
+        addMedicationButton = rootView.findViewById(R.id.add_medication_button);
+        cancelMedicationButton = rootView.findViewById(R.id.add_cancel_medication_button);
+
+        // set spinner adapter to am_pm array
+        spinner.setAdapter(new ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_dropdown_item, am_pm));
+
+        // cancel button click listener
+        cancelMedicationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go back to medication list
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.home_container, new HomeFragment()).commit();
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/s22010170/heathtrakerapp/EditMedicationFragment.java
+++ app/src/main/java/com/s22010170/heathtrakerapp/EditMedicationFragment.java
@@ -7,13 +7,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class EditMedicationFragment extends Fragment {
+
+    Spinner spinner;
+    Button editMedicationButton, cancelEditMedicationButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_edit_medication, container, false);
+        // a.m. or p.m. spinner
+        String[] am_pm = {"a.m", "p.m"};
+        // define variables
+        spinner = rootView.findViewById(R.id.edit_medication_frequency_input_am_pm);
+        editMedicationButton = rootView.findViewById(R.id.edit_medication_button);
+        cancelEditMedicationButton = rootView.findViewById(R.id.edit_cancel_medication_button);
+
+        // set spinner adapter
+        spinner.setAdapter(new ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_dropdown_item, am_pm));
+
+        // cancel button
+        cancelEditMedicationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go back to medication list
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.home_container, new HomeFragment()).commit();
+            }
+        });
 
 
         return rootView;
