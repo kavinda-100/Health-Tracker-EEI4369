d62deab175aead403c794e5d5591cb309ab73e29
kavinda <kavindarathnayake100@gmail.com>
2024-05-15 10:14:02 +0530

    finish the education and report layout

---
+++ app/src/main/java/com/s22010170/heathtrakerapp/EducationalResourcesFragment.java
@@ -0,0 +1,21 @@
+package com.s22010170.heathtrakerapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EducationalResourcesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_educational_resources, container, false);
+
+
+        return rootView;
+    }
+}
