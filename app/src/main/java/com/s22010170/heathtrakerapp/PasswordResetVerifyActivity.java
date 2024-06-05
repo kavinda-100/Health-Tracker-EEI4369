ee08bef676bea8646e270d3591be117a476c4053
kavinda <kavindarathnayake100@gmail.com>
2024-06-05 15:00:55 +0530

    add email verification functionality with OPT email for password reset.

--- app/src/main/java/com/s22010170/heathtrakerapp/PasswordResetVerifyActivity.java
+++ app/src/main/java/com/s22010170/heathtrakerapp/PasswordResetVerifyActivity.java
@@ -2,7 +2,6 @@ package com.s22010170.heathtrakerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,35 +14,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.s22010170.heathtrakerapp.utils.DataBaseHelper;
-import com.s22010170.heathtrakerapp.utils.DbBitmapUtility;
 import com.s22010170.heathtrakerapp.utils.SendEmails;
 import com.s22010170.heathtrakerapp.utils.SharedPrefsManager;
 import com.s22010170.heathtrakerapp.utils.ShowMessage;
 
-public class EmailVerifyActivity extends AppCompatActivity {
+public class PasswordResetVerifyActivity extends AppCompatActivity {
+
     ShowMessage showMessage;
     SharedPrefsManager prefsManager;
     SendEmails sendEmails;
 
-    String MyEmail, MyEmailAppPassword, userEmail, EmailOPT;
-    EditText emailVerifyOPT;
+    String MyEmail, MyEmailAppPassword, userEmail, passwordOPT;
+    EditText passwordVerifyOPT;
     TextView resendEmail;
-    Button verifyEmailButton;
+    Button verifyPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_email_verfy);
+        setContentView(R.layout.activity_password_reset_verfy);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        emailVerifyOPT = findViewById(R.id.email_verify_opt);
-        resendEmail = findViewById(R.id.resend_email_text_view);
-        verifyEmailButton = findViewById(R.id.verify_email_button);
+        passwordVerifyOPT = findViewById(R.id.password_verify_opt);
+        resendEmail = findViewById(R.id.resend_email_text_view_password);
+        verifyPasswordButton = findViewById(R.id.password_button);
         // get the email and password from the strings.xml
         MyEmail = getResources().getString(R.string.my_email);
         MyEmailAppPassword = getResources().getString(R.string.my_email_app_password);
@@ -56,65 +54,63 @@ public class EmailVerifyActivity extends AppCompatActivity {
         showMessage = new ShowMessage();
 
         // get the email from shared preferences
-        EmailOPT = prefsManager.getString("email_otp", null);
-        userEmail = prefsManager.getString("email", null);
+        passwordOPT = prefsManager.getString("password_otp", null);
+        userEmail = getIntent().getStringExtra("email");
 
-        //TODO: handle verify OPT
-        verifyEmailButton.setOnClickListener(new View.OnClickListener() {
+        //TODO: Implement the password reset verification logic
+        verifyPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String OPT = emailVerifyOPT.getText().toString();
+                String OPT = passwordVerifyOPT.getText().toString();
                 // check OPT input is empty
                 if(OPT.isEmpty()){
-                    showMessage.show("Warning", "OPT is empty!", EmailVerifyActivity.this);
+                    showMessage.show("Warning", "OPT is empty!", PasswordResetVerifyActivity.this);
                     return;
                 }
                 // check OPT input is 6 digit
                 if(OPT.length() != 6){
-                    showMessage.show("Warning", "OPT should have 6 digit!", EmailVerifyActivity.this);
+                    showMessage.show("Warning", "OPT should have 6 digit!", PasswordResetVerifyActivity.this);
                     return;
                 }
                 // check EmailOPT is null
-                if(EmailOPT == null){
-                    showMessage.show("Error", "Some thing went wrong. please sign up again!", EmailVerifyActivity.this);
+                if(passwordOPT == null){
+                    showMessage.show("Error", "Some thing went wrong. please sign up again!", PasswordResetVerifyActivity.this);
                     return;
                 }
                 // check OPT are same
-                if(OPT.equals(EmailOPT)){
+                if(OPT.equals(passwordOPT)){
                     // clear OPT
-                    String[] keysToRemove = {"email_otp"};
+                    String[] keysToRemove = {"password_otp"};
                     prefsManager.clearFields(keysToRemove);
-                    // save the email verified status to shared preferences
-                    prefsManager.saveBoolean("isEmailVerified", true);
                     // show toast message
-                    Toast.makeText(EmailVerifyActivity.this, "Signed Up", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(PasswordResetVerifyActivity.this, "Password reset successfully.", Toast.LENGTH_SHORT).show();
                     // go to the email verify activity
-                    Intent intent = new Intent(EmailVerifyActivity.this, HomeActivity.class);
+                    Intent intent = new Intent(PasswordResetVerifyActivity.this, SignInActivity.class);
                     startActivity(intent);
                 }
                 else{
-                    showMessage.show("Error", "Invalid OPT", EmailVerifyActivity.this);
-                    System.out.println("EmailOPT: " + EmailOPT + " OPT: " + OPT);
+                    showMessage.show("Error", "Invalid OPT", PasswordResetVerifyActivity.this);
+                    System.out.println("EmailOPT: " + passwordOPT + " OPT: " + OPT);
                     return;
                 }
             }
         });
 
-        //TODO: resend Email
+        //TODO: Implement the resend email logic
         resendEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // clear previous OPT
-                String[] keysToRemove = {"email_otp"};
+                String[] keysToRemove = {"password_otp"};
                 prefsManager.clearFields(keysToRemove);
                 // generate new OTP
                 String newOPT = sendEmails.getOTP();
                 // subject and message for the email
-                String subject = "Email Verification";
+                String subject = "For Password Reset";
                 String message = "Please verify your email by entering the OTP: " + newOPT + "\n\n" + "Thank you";
                 // check userEmail is null
                 if(userEmail == null){
-                    showMessage.show("Error", "Some thing went wrong. please sign up again!", EmailVerifyActivity.this);
+                    showMessage.show("Error", "Some thing went wrong. please sign up again!", PasswordResetVerifyActivity.this);
                     return;
                 }
                 // send email to the user
@@ -122,19 +118,18 @@ public class EmailVerifyActivity extends AppCompatActivity {
                 // check if the email is sent
                 if (isEmailSent) {
                     // save the OTP to shared preferences
-                    prefsManager.saveString("email_otp", newOPT);
+                    prefsManager.saveString("password_otp", newOPT);
                     // show message
                     showMessage.show("Success",
                             "OPT has send to you email. please check your email for verify you email",
-                            EmailVerifyActivity.this);
+                            PasswordResetVerifyActivity.this);
 
                 } else {
-                    showMessage.show("Error", "Email Not Sent. please try again.", EmailVerifyActivity.this);
+                    showMessage.show("Error", "Email Not Sent. please try again.", PasswordResetVerifyActivity.this);
                     return;
                 }
+
             }
         });
     }
-
-
 }
--- app/src/main/java/com/s22010170/heathtrakerapp/ResetPasswordActivity.java
+++ app/src/main/java/com/s22010170/heathtrakerapp/ResetPasswordActivity.java
@@ -2,11 +2,11 @@ package com.s22010170.heathtrakerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +15,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.s22010170.heathtrakerapp.utils.DataBaseHelper;
+import com.s22010170.heathtrakerapp.utils.SendEmails;
+import com.s22010170.heathtrakerapp.utils.SharedPrefsManager;
 import com.s22010170.heathtrakerapp.utils.ShowMessage;
 
 public class ResetPasswordActivity extends AppCompatActivity {
     DataBaseHelper authDataBaseHelper;
     ShowMessage showMessage;
+    SendEmails sendEmails;
+    SharedPrefsManager prefsManager;
+    String MyEmail, MyEmailAppPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +35,18 @@ public class ResetPasswordActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // get the email and password from the strings.xml
+        MyEmail = getResources().getString(R.string.my_email);
+        MyEmailAppPassword = getResources().getString(R.string.my_email_app_password);
+
+        // create send email object
+        sendEmails = new SendEmails(MyEmail, MyEmailAppPassword);
         // create database
         authDataBaseHelper = new DataBaseHelper(this);
         // create show message object
         showMessage = new ShowMessage();
+        // create shared preferences manager
+        prefsManager = new SharedPrefsManager(this);
         // methods
         backToResetFromToSingUp();
         reset();
@@ -62,10 +75,37 @@ public class ResetPasswordActivity extends AppCompatActivity {
                 boolean isResetPassword = authDataBaseHelper.resetPassword(email.getText().toString(), password.getText().toString());
                 // check if the password is reset
                 if (isResetPassword) {
-                    // show toast message
-                    Toast.makeText(ResetPasswordActivity.this, "Password reset successfully", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(ResetPasswordActivity.this, SignInActivity.class);
-                    startActivity(intent);
+                    // get the OTP
+                    String OPT = sendEmails.getOTP();
+                    // subject and message for the email
+                    String subject = "For Password Reset";
+                    String message = "Please verify your email by entering the OTP: " + OPT + "\n\n" + "Thank you";
+                    // send email to the user
+                    boolean isEmailSent = sendEmails.sendEmailToUser(email.getText().toString(), subject, message);
+                    // check if the email is sent
+                    if (isEmailSent) {
+                        // save the OTP to shared preferences
+                        prefsManager.saveString("password_otp", OPT);
+                        // show message
+                        showMessage.show("Success",
+                                "OPT has send to you email. please check your email for verify you email in next screen.",
+                                ResetPasswordActivity.this);
+                        // wait for 5 seconds
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // go to the email verify activity
+                                Intent intent = new Intent(ResetPasswordActivity.this, PasswordResetVerifyActivity.class);
+                                intent.putExtra("email", email.getText().toString());
+                                startActivity(intent);
+                            }
+                        }, 4000); // 4000 milliseconds = 4 seconds
+
+                    } else {
+                        showMessage.show("Error", "Email Not Sent. please try again.", ResetPasswordActivity.this);
+                    }
+
                 }else{
                     // show toast message
                     showMessage.show("Error", "Invalid email", ResetPasswordActivity.this);
